5f22aa0d55e576b039044f1ed8f810d07e92d07d
William Bruntrager <w.bruntrager@gmail.com>
2026-01-17 13:23:00 +0100

    Update knex config'

--- database/knexfile.js
+++ database/knexfile.js
@@ -1,19 +1,26 @@
 // Update with your config settings.
 const path = require('path')
 
+const commonConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: path.join(__dirname, 'holdem.sqlite3'),
+  },
+  useNullAsDefault: true,
+  migrations: {
+    directory: path.join(__dirname, 'migrations'),
+  },
+}
+
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 const config = {
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: path.join(__dirname, 'holdem.sqlite3'),
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: path.join(__dirname, 'migrations'),
-    },
+    ...commonConfig,
+  },
+  staging: {
+    ...commonConfig,
   },
   test: {
     client: 'sqlite3',
